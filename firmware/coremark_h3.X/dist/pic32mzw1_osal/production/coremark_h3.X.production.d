06e83750e6a0cb39fdc580a6b166db788d5f0bf9
Vysakh Pillai <vysakh.pillai@microchip.com>
2020-12-30 10:40:46 +0530

    .

---
+++ firmware/coremark_h3.X/dist/pic32mzw1_osal/production/coremark_h3.X.production.d
@@ -0,0 +1,2 @@
+dist/pic32mzw1_osal/production/coremark_h3.X.production.elf: \
+ c:/program\ files/microchip/xc32/v2.40/bin/bin/../../lib/gcc/pic32mx/4.8.3/../../../../pic32mx/lib/./proc/32MZ1025W104132/p32MZ1025W104132_div.S
